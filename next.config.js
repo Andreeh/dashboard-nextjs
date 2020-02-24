740ce15c0c2a3a5cca3c330b2cf512af191d3610
Andrée Höög <Hoog16@gmail.com>
2020-02-25 00:38:07 +0100

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withSass = require('@zeit/next-sass');
-
-module.exports = withSass({
-    cssModules: true,
-    cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: '[local]___[hash:base64:5]'
-    }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
 });
+
+module.exports = withBundleAnalyzer(
+    withSass({
+        cssModules: true,
+        cssLoaderOptions: {
+            importLoaders: 1,
+            localIdentName: '[local]___[hash:base64:5]'
+        }
+    })
+);
